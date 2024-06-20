6fe5eed3a264884b6e3f1c30e5e18b240a9c1fd9
BumbertoEko <118741303+BumbertoEko@users.noreply.github.com>
2024-06-20 03:06:00 +0300

    Added Player
    Added Player to the Main scene
    with movement script

---
+++ CharacterBody2D.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+#-------------------------------------------------------------------------------- movement
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _physics_process(_delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var direction_y = Input.get_axis("Up", "Down")
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	
+	velocity = velocity.normalized() * SPEED
+	move_and_slide()
+	
+	#--------------------------------------------------- movement
